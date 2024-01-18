a997c7b4345dc6d5235e86c20ed9bd7d201e94b7
Tony-Ton3 <tongyuzhu1026@gmail.com>
2024-01-18 13:59:19 -0800

    added functionality to signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,45 @@
-import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { Button, Label, TextInput, Alert, Spinner } from 'flowbite-react';
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null); 
+  const [loading, setLoading] = useState(false); 
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: String(e.target.value.trim()) }); //.id represent a input box, .value is the user input
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); //prevent refresh everytime signup button is clicked
+
+    if(!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill in all fields');
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null); //clear error message when page is rerendered 
+      const res = await fetch('/api/auth/signup', { //backend url is valid because we set proxy in vite.config.js
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }, //using json data
+            body: JSON.stringify(formData) //convert json to string before making api call to mongodb
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        Navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  }
+
   return (
     <div className='min-h-screen mt-20'> 
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -22,22 +59,31 @@ export default function SignUp() {
 
         {/* input side */}
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value = 'Your username' />
-              <TextInput type='text' placeholder='Username' id='username'/>
+              <TextInput type='text' placeholder='Username' id='username' onChange={handleChange}/>
             </div>
             <div>
               <Label value = 'Your email' />
-              <TextInput type='text' placeholder='example@company.com' id='email'/>
+              <TextInput type='email' placeholder='example@company.com' id='email' onChange={handleChange}/>
             </div>
             <div>
               <Label value = 'Your password' />
-              <TextInput type='text' placeholder='Password' id='password'/>
+              <TextInput type='password' placeholder='Password' id='password' onChange={handleChange}/>
             </div>
-            <Button className='bg-gradient-to-r from-emerald-500 to to-black' type='submit'>
-              Sign Up
-            </Button>
+            <div className='flex justify-center'>
+              <Button className='w-80 bg-gradient-to-r from-emerald-500 to to-black' type='submit' disabled={loading}>
+                {loading ? (
+                    <>
+                      <Spinner size='sm' />
+                      <span className='pl-3'> Loading... </span>
+                    </> 
+                    ) : (
+                      'Sign up'
+                    )}
+              </Button>
+            </div>
           </form>
           <div className='flex gap-2 text-sm mt-5'> 
           <span>Have an account?</span>
@@ -45,6 +91,11 @@ export default function SignUp() {
               Sign in
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
         </div>
         
       </div>
