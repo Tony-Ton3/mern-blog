6cb1f933ef8b8bc8b61d2c211625ed80c9628e3c
Tony-Ton3 <tongyuzhu1026@gmail.com>
2024-01-20 14:18:20 -0800

    added tab navigation within dashboard and sidebar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import { useLocation } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import DashSidebar from '../components/DashSidebar'
+import DashProfile from '../components/DashProfile'
 
 export default function Dashboard() {
+  const location = useLocation()
+  const [tab, setTab] = useState('')
+
+  //allows navigation to different tabs within dashboard page. ex: /dashboard?tab=profile
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    if(tabFromUrl) {
+      setTab(tabFromUrl)
+    }
+  }, [location.search]);
   return (
-    <div>Dashboard</div>
+    <div className="min-h-screen md:flex-row flex flex-col">
+      <div className='md:w-56'>
+       {/* sidebar */}
+       <DashSidebar />
+      </div>
+      {/* profile */}
+      {tab === 'profile' && <DashProfile />} 
+    </div>
   )
+  
 }
